c94638a59b2f293e782a274cd88eb4f0798e4da9
tswartz <tswartz@hubspot.com>
2016-04-05 15:16:30 -0400

    api request and set dom, dont add appKey.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,19 @@
+$(document).ready(function() {
+  getWeather();
+});
 
-function myFunction() {
-  document.getElementById("demo").innerHTML = "NEWWW PARAGRAPH #javascript #swag";
+function getWeather() {
+  var url = 'http://api.openweathermap.org/data/2.5/weather?id=4930956&units=imperial&APPID=' + appKey;
+  $.ajax(url).done(function (data) {
+    $('.city').text(data.name);
+    $('.temp').text(data.main.temp);
+    $('.summary').html(parseSummary(data));
+  });
 }
 
+function parseSummary(data) {
+  var description = data.weather[0].description;
+  var icon = data.weather[0].icon;
+  var iconSrc = "http://openweathermap.org/img/w/" + icon + ".png";
+  return `<span>${description}</span><img src="${iconSrc}"/>`;
+}
