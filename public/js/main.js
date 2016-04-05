a260aef3acee2e1646a90c3ceadce53465be9b1b
tswartz <tswartz@hubspot.com>
2016-04-05 15:39:39 -0400

    Added search, break up url into pieces and build it

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,23 @@ $(document).ready(function() {
   getWeather();
 });
 
-function getWeather() {
-  var url = 'http://api.openweathermap.org/data/2.5/weather?id=4930956&units=imperial&APPID=' + appKey;
-  $.ajax(url).done(function (data) {
+function searchWeather() {
+  var searchQuery = $('.search').val();
+  getWeather(searchQuery);
+}
+
+function getWeather(searchQuery) {
+  var url = 'http://api.openweathermap.org/data/2.5/weather?';
+  var params = {
+    APPID: appKey, // this is being pulled in from public/js/appKey.js which I have hidden from Git, don't expose API keys on GitHub!
+    units: 'imperial' // let's use fahrenheit. murica!
+  };
+  if (searchQuery) {
+    params.q = searchQuery;
+  } else {
+    params.id = 4930956 // id for Boston, you can find city codes here http://bulk.openweathermap.org/sample/
+  }
+  $.ajax(url + $.param(params)).done(function (data) {
     $('.city').text(data.name);
     $('.temp').text(data.main.temp);
     $('.summary').html(parseSummary(data));
@@ -14,6 +28,6 @@ function getWeather() {
 function parseSummary(data) {
   var description = data.weather[0].description;
   var icon = data.weather[0].icon;
-  var iconSrc = "http://openweathermap.org/img/w/" + icon + ".png";
+  var iconSrc = `http://openweathermap.org/img/w/${icon}.png`;
   return `<span>${description}</span><img src="${iconSrc}"/>`;
 }
