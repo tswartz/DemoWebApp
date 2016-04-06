c8ceb10816ed61bfbaf260f912affcdd4e49b004
tswartz <tswartz@hubspot.com>
2016-04-06 10:50:14 -0400

    nevermind

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var request = require('request');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -10,10 +9,8 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, response) {
-	request('http://api.openweathermap.org/data/2.5/weather?id=4930956&APPID=' + process.env.API_KEY, function(error, resp, body) {
-		response.render('pages/index', {body: body});
-	});
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
